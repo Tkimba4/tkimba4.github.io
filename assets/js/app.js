b22116c764f4b33e8751f323461a9a8f99067e07
Tkimba4 <tkimba4.dev@gmail.com>
2025-03-17 13:50:12 +0100

    header update

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,32 +1,39 @@
 document.addEventListener("DOMContentLoaded", () => {
   //Nav - open/close
   (() => {
-    const btnMenu = document.querySelector("#navMenu button");
-    const links = document.querySelector(".nav-links");
-    btnMenu.addEventListener("click", (e) => {
-      e.currentTarget.classList.toggle("show");
-      links.classList.toggle("show");
-    });
+    const showMenu = (toggleId, navId) => {
+      const toggle = document.getElementById(toggleId),
+        nav = document.getElementById(navId);
 
+      if (toggle && nav) {
+        toggle.addEventListener("click", () => {
+          nav.classList.toggle("show");
+          toggle.classList.toggle("show");
+        });
+      }
+    };
+    const links = document.querySelector(".nav-links");
     links.querySelectorAll("a").forEach((link) => {
       link.addEventListener("click", (e) => {
         e.preventDefault();
-        const el = link.getAttribute("href").replace('#', "");
+        const el = link.getAttribute("href").replace("#", "");
         document.getElementById(el).scrollIntoView();
       });
     });
+    showMenu("btn-menu", "nav-menu");
   })();
 
   //project section
-  (()=>{
-    const btnServicesContact = document.querySelector('.start-project-btn');
-    btnServicesContact.addEventListener('click', ()=>{document.getElementById('contact').scrollIntoView()})
+  (() => {
+    const btnServicesContact = document.querySelector(".start-project-btn");
+    btnServicesContact.addEventListener("click", () => {
+      document.getElementById("contact").scrollIntoView();
+    });
   })();
 
   //footer section
-  (()=>{
+  (() => {
     const date = new Date().getFullYear();
-    document.querySelector('#footer-date').textContent = date;
+    document.querySelector("#footer-date").textContent = date;
   })();
 });
-
